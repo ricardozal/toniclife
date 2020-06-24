1d511e8f9439fd1f6e5fc1bb4a05c5def2295210
Ricardo Zaldivar Pichardo <ricardozalpich@gmail.com>
2020-06-23 23:08:57 -0500

    Fix Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,24 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->truncateTables([
-            'country',
-            'product'
-        ]);
 
         $this->call(UserSeeder::class);
         $this->call(CountrySeeder::class);
         $this->call(ProductsMexSeeder::class);
+        $this->call(ProductsSeeder::class);
     }
 
-    public function truncateTables(array $tables)
-    {
-        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
-
-        foreach ($tables as $table){
-            DB::table($table)->truncate();
-        }
-
-        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
-    }
 }
