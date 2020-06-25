d0d00f51172e5f128af482ecfd1db35d970d9444
geovanniayala <geovanni.ayalapea@gmail.com>
2020-06-25 00:33:02 -0500

    primer seeder Euu prueba

--- database/seeds/Products3Seeder.php
+++ database/seeds/Products3Seeder.php
@@ -1,7 +1,24 @@
 <?php
+use \Illuminate\Database\Seeder;
+use \Illuminate\Support\Facades\DB;
 
-
-class Products3Seeder
+class Products3Seeder extends Seeder
 {
+
+    public function run()
+    {
+        DB::table('product')->insert([
+            [
+                'code'=>'6821',
+                'name'=>'FORMULA COLOIDAL SPRAY',
+                'image_url'=>'products/6821.jpg',
+                'distributor_price'=>180,
+                'points'=>180,
+                'fk_id_country' => 2,
+
+
+            ]
+        ]);
+    }
 
 }
