bbf45f46f3ead5c32d9325664335695c3bf51f7b
Ricardo Zaldivar Pichardo <ricardozalpich@gmail.com>
2020-06-28 11:41:27 -0500

    Fix seeder

--- database/seeds/BranchHasProductsSeeder.php
+++ database/seeds/BranchHasProductsSeeder.php
@@ -8,21 +8,21 @@ class BranchHasProductsSeeder extends Seeder
     public function run()
     {
         DB::table('branch_has_products')->insert([
-            'stock'=>'5',
+            'stock'=>5,
             'fk_id_product'=>1,
             'fk_id_branch'=>1,
         ]);
 
         DB::table('branch_has_products')->insert([
-            'stock'=>'5',
+            'stock'=>5,
             'fk_id_product'=>2,
-            'fk_id_branch'=>5,
+            'fk_id_branch'=>1,
         ]);
 
         DB::table('branch_has_products')->insert([
-            'stock'=>'5',
+            'stock'=>5,
             'fk_id_product'=>3,
-            'fk_id_branch'=>6,
+            'fk_id_branch'=>1,
         ]);
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call(OrderStatusSeeder::class);
         $this->call(PaymentMethodSeeder::class);
         $this->call(OrderSeeder::class);
+        $this->call(BranchHasProductsSeeder::class);
     }
 
 }
