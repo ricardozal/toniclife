90b68f3bba67bd40cd6e9980cc843ba250c93679
Ricardo Zaldivar Pichardo <ricardozalpich@gmail.com>
2020-07-13 20:58:38 -0500

    Some Fixes

--- resources/views/admin/inventory/global/index.blade.php
+++ resources/views/admin/inventory/global/index.blade.php
@@ -36,7 +36,7 @@
     <input type="hidden" value="{{route('admin_inventory_global_showMovements',['fk_id_product' => 'FAKE_ID'])}}" id="inp-url-showMovements">
 
     <div id='modal-upsert' class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog modal-xl modal-dialog-centered">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-body">
                     <div class="body-content"></div>
--- resources/views/admin/inventory/global/show.blade.php
+++ resources/views/admin/inventory/global/show.blade.php
@@ -2,17 +2,17 @@
     <h4 class="text-header mt-2">Producto {{$product->name}}</h4>
 </div>
 
-<div class="row">
-    <div class="col-12 p-md-5 py-1">
+<div class="row w-75 mx-auto">
+    <div class="col-12 px-md-5 py-1">
         <div class="row">
             <div class="col-6">
-                <p class="font-weight-bold">Codigo {{$product->code}}</p>
+                <p><strong>Código: </strong> {{$product->code}}</p>
             </div>
-            <div class="col-6 text-right">
-                <p class="font-weight-bold">Catalogo {{$product->country->name}}</p>
+            <div class="col-6">
+                <p><strong>Catálogo: </strong> {{$product->country->name}}</p>
             </div>
         </div>
-        <div class="row">
+        <div class="row w-75 text-center mx-auto">
             <table class="table table-borderless color-light-dark table-responsive">
                 <thead class="border-bottom">
                 <tr>
