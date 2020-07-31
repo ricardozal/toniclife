e327b84d5eea0b0fa4ea7bbd1679648ae30dc42a
Ricardo Zaldivar Pichardo <ricardozalpich@gmail.com>
2020-07-31 18:51:13 -0500

    WhatsApp Message Structure

--- app/Notifications/OrderProcessed.php
+++ app/Notifications/OrderProcessed.php
@@ -29,9 +29,28 @@ class OrderProcessed extends Notification
 
     public function toWhatsApp($notifiable)
     {
-        $distributorName = $this->order->distributor->name;
+        $distributor = $this->order->distributor;
+
+        $listOfProducts = '';
+
+        $currency = $this->order->branch->address->country->id == 1 ? "MXN" : "US";
+
+        foreach ($this->order->products as $product){
+
+            $listOfProducts .= $product->pivot->quantity." ".$product->name." ".$currency.number_format($product->pivot->price,2)."\n";
+
+        }
+
+        $message = $distributor->name." ".$distributor->tonic_life_id."\n".
+                   "\n".
+                    $listOfProducts.
+                   "\n".
+                   "Total:\n".
+                   "Precio distribuidor: ".$currency.number_format($this->order->total_price,2)."\n".
+                   "Tax: ".$currency.number_format($this->order->total_taxes,2)."\n".
+                   "Puntos: ".$this->order->total_accumulated_points;
 
         return (new WhatsAppMessage)
-            ->content("Tu orden ha sido procesada, {$distributorName}");
+            ->content("{$message}");
     }
 }
